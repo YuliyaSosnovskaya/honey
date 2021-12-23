855fc1b9164efe0a5acff8f3ccdcfbcd7066c539
Yuliya <yuliya55122@gmail.com>
2021-12-23 04:12:20 +0300

    fix layout

--- webpack.config.js
+++ webpack.config.js
@@ -6,23 +6,33 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    assetModuleFilename: 'img/[name][ext]',
+    // Specify loaders for images.
+    assetModuleFilename: 'imgs/[name][hash][ext]',
     clean: true,
   },
   module: {
     rules: [
       {
-        test: /\.(css)$/,
-        use: ['style-loader','css-loader'],
-      },
-      {
-        test: /\.(s(a|c)ss)$/,
+        test: /\.((sa|sc|c)ss)$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
+      // By default every loadable attributes (for example - <img src="image.png"/>) is imported.
+      // See more details here https://webpack.js.org/loaders/html-loader
       {
-        test: /\.(png|jpg)$/,
-        type: 'asset/resource',
+        test: /\.html$/,
+        use: ["html-loader"]
       },
+      // We don't need "file-loader" here. "html-loader" loads all the images
+      // {
+      //   test: /\.(png|jpg|svg|gif)$/,
+      //   use: {
+      //     loader: 'file-loader',
+      //     options: {
+      //       name: '[name].[hash].[ext]',
+      //       outputPath: 'img',
+      //     },   
+      //   },
+      // },
     ]
   },
   plugins: [new HtmlWebpackPlugin({template: './index.html'})]
